2baedaf206b479648130e40acc8a71ed53b300c2
Vemugunta, Sri (sv870e) <sv870e@att.com>
2022-06-30 15:23:44 -0500

    Added router Dom

--- teamwinners/src/App.js
+++ teamwinners/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
+import { BrowserRouter ,Route, Routes } from 'react-router-dom';
 function App() {
   return (
-   <div>
-    <Login/>
-   </div>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Login />} />
+      <Route exact path="/login" element={<div>hello</div>} />
+      <Route exact path="/signUp" element={<div>hello2</div>} />
+
+    </Routes>
+  </BrowserRouter>
   );
 }
 
